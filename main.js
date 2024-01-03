2c8759cf5e3ba6396bec918be2e40eb53a1d6d62
Ghimel Cândido <105327320+ghimelcandido@users.noreply.github.com>
2024-01-03 18:56:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 
-function imgClosedCookie(){
+function openTheCookie(event){
+    event.preventDefault()
+
     screen1.classList.add("hide")
     screen2.classList.remove("hide")
 }
 
-function openAnotherOne(){
+function openAnotherOne(event){
+    event.preventDefault()
+
     location.reload()
 }
+
+const imgClosedCookie = document.querySelector("#imgClosedCookie")
+const getAnotherCookie = document.querySelector("#openAnotherOne")
+
+imgClosedCookie.addEventListener('click', openTheCookie)
+getAnotherCookie.addEventListener('click', openAnotherOne)
